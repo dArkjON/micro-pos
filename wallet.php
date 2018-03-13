43a07550e16c591b208776445f6efd182b7c7f48
Jon D <info@darkjon.de>
2018-03-13 18:47:55 +0100

    Update wallet.php

--- wallet.php
+++ wallet.php
@@ -4,5 +4,4 @@ $wallet1 = '13R4ZRMFfTysscP2K1krZaNZNJgF9jTiCJ';
 $wallet2 = '17CJJC4J3bqeF479x3ihRUcodYJhvvjTWA';
 $wallet3 = '18QFyvYczodKetMRgioyZSB4ARbby5Ac4';
 $wallet4 = '1ByedRWzXbpbjErBcM8eR5XPS7tVJVoiLs';
-
 ?>

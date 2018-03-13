a5977a7cb5fe2f74be038a4948cba18009c2ea55
dArkjON <info@darkjon.de>
2018-03-13 18:49:53 +0100

    fix
    fix

--- wallet.php
+++ wallet.php
@@ -1,7 +1,9 @@
 ﻿<?php
+
 $wallet = '11472JagyCb2sXEqHNBdm3KD22S27P7FC4';
 $wallet1 = '13R4ZRMFfTysscP2K1krZaNZNJgF9jTiCJ';
 $wallet2 = '17CJJC4J3bqeF479x3ihRUcodYJhvvjTWA';
 $wallet3 = '18QFyvYczodKetMRgioyZSB4ARbby5Ac4';
 $wallet4 = '1ByedRWzXbpbjErBcM8eR5XPS7tVJVoiLs';
+
 ?>

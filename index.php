b960e93506d60d6ac51ac0220bed1a31bdf064ef
Jon D <info@darkjon.de>
2018-03-23 19:58:09 +0100

    Update index.php
    add https support

--- index.php
+++ index.php
@@ -50,7 +50,7 @@ echo '</form>';
 }else{
 $target = number_format($price/$peuro, 8);
 	
-$json2 = file_get_contents('http://insight.bitcore.cc/api/addr/'.$wallet); 
+$json2 = file_get_contents('https://insight.bitcore.cc/api/addr/'.$wallet); 
 $data2 = json_decode($json2);
 $addrStr = $data2->addrStr;
 $balance = $data2->balance;
@@ -63,7 +63,7 @@ echo '<h6>Invoice total amount of : '.$price.' €<br />'.$target.' '.$psymbole.
 echo '<a href="bitcore:'.$wallet.'?amount='.$target.'"><img src="https://chart.googleapis.com/chart?chs=200x200&cht=qr&chl=bitcore:'.$wallet.'?amount='.$target.'" title="'.$wallet.'" /></a>';
 echo '<div id="open"><h6>Payment Wallet : '.$wallet.'<br><br>... loading Details ...</h6></div>';
 }else{
-	$json2 = file_get_contents('http://insight.bitcore.cc/api/addr/'.$wallet1); 
+	$json2 = file_get_contents('https://insight.bitcore.cc/api/addr/'.$wallet1); 
 	$data2 = json_decode($json2);
 	$addrStr = $data2->addrStr;
 	$balance = $data2->balance;
@@ -75,7 +75,7 @@ echo '<div id="open"><h6>Payment Wallet : '.$wallet.'<br><br>... loading Details
 	echo '<a href="bitcore:'.$wallet1.'?amount='.$target.'"><img src="https://chart.googleapis.com/chart?chs=300x300&cht=qr&chl=bitcore:'.$wallet1.'?amount='.$target.'" title="'.$wallet1.'" /></a>';
 	echo '<div id="open"><h6>Payment Wallet : '.$wallet1.'<br><br>... loading Details ...</h6></div>';
 	}else{
-	$json2 = file_get_contents('http://insight.bitcore.cc/api/addr/'.$wallet2); 
+	$json2 = file_get_contents('https://insight.bitcore.cc/api/addr/'.$wallet2); 
 	$data2 = json_decode($json2);
 	$addrStr = $data2->addrStr;
 	$balance = $data2->balance;
@@ -87,7 +87,7 @@ echo '<div id="open"><h6>Payment Wallet : '.$wallet.'<br><br>... loading Details
 	echo '<a href="bitcore:'.$wallet2.'?amount='.$target.'"><img src="https://chart.googleapis.com/chart?chs=300x300&cht=qr&chl=bitcore:'.$wallet2.'?amount='.$target.'" title="'.$wallet2.'" /></a>';
 	echo '<div id="open"><h6>Payment Wallet : '.$wallet2.'<br><br>... loading Details ...</h6></div>';
 	}else{
-	$json2 = file_get_contents('http://insight.bitcore.cc/api/addr/'.$wallet3); 
+	$json2 = file_get_contents('https://insight.bitcore.cc/api/addr/'.$wallet3); 
 	$data2 = json_decode($json2);
 	$addrStr = $data2->addrStr;
 	$balance = $data2->balance;
@@ -99,7 +99,7 @@ echo '<div id="open"><h6>Payment Wallet : '.$wallet.'<br><br>... loading Details
 	echo '<a href="bitcore:'.$wallet3.'?amount='.$target.'"><img src="https://chart.googleapis.com/chart?chs=300x300&cht=qr&chl=bitcore:'.$wallet3.'?amount='.$target.'" title="'.$wallet3.'" /></a>';
 	echo '<div id="open">Payment Wallet : '.$wallet3.'<br><br>... loading Details ...</div>';
 	}else{
-	$json2 = file_get_contents('http://insight.bitcore.cc/api/addr/'.$wallet4); 
+	$json2 = file_get_contents('https://insight.bitcore.cc/api/addr/'.$wallet4); 
 	$data2 = json_decode($json2);
 	$addrStr = $data2->addrStr;
 	$balance = $data2->balance;
